60ff6934390b745679dc7de018668d0520ea7ec6
tareqrahaman <tareqrahaman565@gmail.com>
2024-06-27 08:37:50 +0600

    updated

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#taking input from the user
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+num3 = float(input("Enter the 3rd number: "))
+
+#printing output
+print("\n________ Results ________")
+print(f"Summation is      = {num1+num2+num3:.2f}")
+print(f"Subtraction is    = {num1-num2-num3:.2f}")
+print(f"Multiplication is = {num1*num2*num3:.2f}")
+print(f"Division is       = {num1/num2/num3:.2f}")
+
