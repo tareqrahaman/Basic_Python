335373afb5c88ea38732def491c6ee9af88146f8
tareqrahaman <tareqrahaman565@gmail.com>
2024-06-30 13:15:17 +0600

    added string-indexing

---
+++ string-indexing.py
@@ -0,0 +1,13 @@
+example_string = "Python is fun!"
+
+#finding length
+length = len(example_string)
+
+#pirnting out the entire string
+print(example_string)
+
+#print individual characters using Positive indexing
+print(example_string[0:length])
+
+#print individual characters using negative indexing
+print(example_string[-1:-(length+1):-1])
