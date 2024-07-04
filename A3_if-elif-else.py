c3461e0f8df7192b2410693eb99301f6e080b50a
tareqrahaman <tareqrahaman565@gmail.com>
2024-07-04 18:48:59 +0600

    assignment 3 added

---
+++ A3_if-elif-else.py
@@ -0,0 +1,36 @@
+#taking user input
+num = int(input("Enter an integer number: "))
+
+#task1
+print("\n____Task1____")
+if num>0: 
+    print(f"{num} is positive.")
+elif num<0:
+    print(f"{num} is negative.")
+else: 
+    print(f"{num} is zero.")
+print()
+
+#task2
+print("__Task2__")
+if num%2==0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
+print()
+
+#task3
+print("__Task3__")
+if num>100 or num<0:
+    print("Invalid value.")
+elif num>=90:
+    print("Grade: A")
+elif num>=80:
+    print("Grade: B")
+elif num>=70:
+    print("Grade: C")
+elif num>=60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+print()
